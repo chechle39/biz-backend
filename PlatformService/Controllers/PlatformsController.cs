ff5002743a2d7439c1d85c7d444843358d1adc00
Tuan-Le <chechle3979@gmail.com>
2024-12-31 16:54:48 +0700

    add platforms service project (#1)

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _platformRepo;
+        private readonly IMapper _mapper;
+        public PlatformsController(IPlatformRepo platformRepo, IMapper mapper)
+        {
+            _platformRepo = platformRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("->> Getting platforms...");
+            var platformItem = _platformRepo.GetAllPlatform();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id) 
+        {
+            Console.WriteLine("->> Getting platforms by id...");
+            var platformItem = _platformRepo.GetPlatformById(id);
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platform)
+        {
+            var map = _mapper.Map<Platform>(platform);
+            _platformRepo.CreatePlatform(map);
+            _platformRepo.SaveChanges();
+            var platformReadDto = _mapper.Map<PlatformReadDto>(map);
+            var route = CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+            return route;
+        }
+    }
+}
---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,56 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+
+        }
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding data...");
+                context.Platforms.AddRange(
+                    new Platform()
+                    {
+                        Name = "dot net",
+                        Publisher = "mricrosoft",
+                        Cost = "free",
+                        Description = "test app",
+                        //Id = 1
+                    },
+                    new Platform()
+                    {
+                        Name = "sql",
+                        Publisher = "mricrosoft",
+                        Cost = "free",
+                        Description = "test app",
+                        //Id = 2
+                    },
+                    new Platform()
+                    {
+                        Name = "K8s",
+                        Publisher = "mricrosoft",
+                        Cost = "free",
+                        Description = "test app",
+                        //Id = 1
+                    }
+                );
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> we alredy have data");
+            }
+
+        }
+
+    }
+}
