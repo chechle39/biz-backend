db58df2816bfa0a8538b3b5f419e2c8a7804a7a2
TuanLe <chechle3979@gmail.com>
2025-01-01 23:17:23 +0700

    call commandservice from platformservice with SynchronousMessaging

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,22 +1,14 @@
-using Microsoft.EntityFrameworkCore;
-using PlatformService.Data;
-using PlatformService.SyncDataServices.Http;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
-builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 
 var app = builder.Build();
-PrepDb.PrepPopulation(app);
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
