1a5d92c268b5e5ad4a64ba21e0e75f9dc4effa9e
TuanLe <chechle3979@gmail.com>
2025-01-05 17:56:44 +0700

    add datalayer dto

---
+++ CommandsService/Dtos/CommandReadDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CommandsService.Dtos
+{
+    public class CommandReadDto
+    {
+        public int Id { get; set; }
+        public string Howto { get; set; } = null!;
+        public string CommandLine { get; set; } = null!;
+        public int PlatformId { get; set; }
+    }
+}
