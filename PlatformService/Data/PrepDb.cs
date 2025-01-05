b48ec02cd3504cd7a3bb5b33d2836b181e903b20
TuanLe <chechle3979@gmail.com>
2025-01-05 17:02:26 +0700

    create database for platforms and add migration

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,19 +1,33 @@
-﻿using PlatformService.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
 
 namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app, bool IsProduction)
         {
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), IsProduction);
             }
 
         }
-        private static void SeedData(AppDbContext context)
+        private static void SeedData(AppDbContext context, bool IsProduction)
         {
+            if (IsProduction)
+            {
+                Console.WriteLine("--> apply migrations...");
+                try
+                {
+                    context.Database.Migrate();
+
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine($"--> could not run migration: {ex.Message}");
+                }
+            }
             if (!context.Platforms.Any())
             {
                 Console.WriteLine("--> Seeding data...");
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -7,16 +7,34 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+//Configure the HTTP request pipeline.
+if (builder.Environment.IsProduction())
+{
+
+    Console.WriteLine("--> Using sql server Db");
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("platformsConn"))
+    );
+
+} else
+{
+    Console.WriteLine("--> Using inMem Db");
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+
+}
+
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
-
 var app = builder.Build();
-PrepDb.PrepPopulation(app);
+PrepDb.PrepPopulation(app, app.Environment.IsProduction());
+
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
@@ -24,13 +42,6 @@ app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
     options.RoutePrefix = string.Empty;
 });
-//Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-
-}
-
 // app.UseHttpsRedirection();
 
 app.UseRouting();
