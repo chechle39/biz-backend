6507d53332ff09764b98a5e86b692ef98810ef4b
MOON <chechle3979@gmail.com>
2025-01-12 10:43:24 +0700

    add a grpc  client to command service (#15)

---
+++ CommandsService/SyncDataServices/Grpc/PlatformDataClient.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using CommandsService.Models;
+using Grpc.Net.Client;
+using PlatformService;
+
+namespace CommandsService.SyncDataServices.Grpc
+{
+    public class PlatformDataClient : IPlatformDataClient
+    {
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+        public PlatformDataClient(IConfiguration configuration, IMapper mapper) 
+        { 
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+        public IEnumerable<Platform> ReturnAllPlatforms()
+        {
+            Console.WriteLine($"--> Calling GRPC Service {_configuration["GrpcPlatform"]}");
+            var channel = GrpcChannel.ForAddress(_configuration["GrpcPlatform"]);
+            var client = new GrpcPlatform.GrpcPlatformClient(channel);
+            var request = new GetAllRequest();
+            try
+            {
+                var reply = client.GetAllPlatforms(request);
+                var map = _mapper.Map<IEnumerable<Platform>>(reply.Plaform);
+                return map;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Couldnot call GRPC Server");
+                return null;
+            }
+        }
+    }
+}
